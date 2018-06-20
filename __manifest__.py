8f021404c3c295c370b77492debafaeb98399878
Romanishen Vasya <v.romanishen@example.com>
2018-06-20 16:34:35 +0300

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name':'My Snippet',
+    'description':'My Snippet',
+    'category': 'Website',
+    'version':'1.1',
+    'author':'Romanishen Vasya',
+    'data': [
+        'views/assets.xml',
+        'views/snippets.xml',
+        'views/body.xml',
+    ],
+    'depends': [],
+}
